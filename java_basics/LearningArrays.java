29c53b046b9ef9eacfaf0ba72412d43d2ff390f4
lisa000s <vasilinka86@gmail.com>
2020-11-06 17:07:59 -0500

    Unit1

---
+++ java_basics/LearningArrays.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class LearningArrays {
+
+	public static void main(String[] args) {
+		int [] values = new int[100];
+		values[2] =1000;
+		
+//System.out.println(values[2]);
+String[]words = new String[] {"My","name","is"};
+//System.out.println(words[2]);
+words = new String[10];
+System.out.println(words[2]);
+	}
+	}
---
+++ java_basics/Variables.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int x = 4;
+		System.out.println(x);
+		x = 6;
+		System.out.println(x);
+		String wrds = "Hello";
+		System.out.println(wrds);
+	}
+
+}
