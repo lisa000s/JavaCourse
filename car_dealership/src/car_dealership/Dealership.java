cdadbf201d9be00eac26e5795ad56aea75acebcc
Li <vasilinka86@gmail.com>
2020-11-13 12:05:26 -0500

    push car

---
+++ car_dealership/src/car_dealership/Dealership.java
@@ -0,0 +1,14 @@
+package car_dealership;
+
+public class Dealership {
+
+	public static void main(String[] args) {
+		Customer cust1 = new Customer();
+		cust1.setName("Tom");
+		cust1.setAddress("123 Apple St");
+		cust1.setCashOnHand(2000);
+		cust1.purchaseCar(vehicle, emp, finance);
+
+	}
+
+}
